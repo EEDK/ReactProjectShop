fc718fcbf6736e5457a38fdd6fa152b48e9fe107
EDK <kde9604@naver.com>
2020-05-31 00:13:33 +0900

    addHeader&FooterBaseCode

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <header>
+        <h1>보다 나은 세상</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <h1>보다 나은 세상</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
